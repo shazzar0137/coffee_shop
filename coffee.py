2bbb143fb80d45b394e643ec2a5ebb7081613e9d
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-05-18 20:11:08 +0300

    updates customer.py

--- coffee.py
+++ coffee.py
@@ -1,12 +1,12 @@
 
 from order import Order
 
-class Coffee:
+class Customer:
     _all = []
 
     def __init__(self, name):
         self.name = name
-        Coffee._all.append(self)
+        Customer._all.append(self)
 
     @property
     def name(self):
@@ -14,22 +14,27 @@ class Coffee:
 
     @name.setter
     def name(self, value):
-        if isinstance(value, str) and len(value) >= 3:
+        if isinstance(value, str) and 1 <= len(value) <= 15:
             self._name = value
         else:
-            raise ValueError("Coffee name must be a string with at least 3 characters.")
+            raise ValueError("Customer name must be a string between 1 and 15 characters.")
 
     def orders(self):
-        return [order for order in Order._all if order.coffee == self]
+        return [order for order in Order._all if order.customer == self]
 
-    def customers(self):
-        return list(set(order.customer for order in self.orders()))
+    def coffees(self):
+        return list(set(order.coffee for order in self.orders()))
 
-    def num_orders(self):
-        return len(self.orders())
+    def create_order(self, coffee, price):
+        return Order(self, coffee, price)
 
-    def average_price(self):
-        orders = self.orders()
-        if not orders:
-            return 0
-        return sum(order.price for order in orders) / len(orders)
+    @classmethod
+    def most_aficionado(cls, coffee):
+        max_spent = 0
+        top_customer = None
+        for customer in cls._all:
+            spent = sum(order.price for order in customer.orders() if order.coffee == coffee)
+            if spent > max_spent:
+                max_spent = spent
+                top_customer = customer
+        return top_customer
